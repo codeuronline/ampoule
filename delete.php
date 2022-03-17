43ea21e3e5ea511da9adf8570b5d1c4781e466ae
codeuronline <j.kasperski@codeur.online>
2022-03-17 15:45:19 +0100

    update

--- delete.php
+++ delete.php
@@ -5,20 +5,19 @@ session_start();
 
 if($_SESSION['ask']){
     $_SESSION['id'] = @$_GET['id'];
-    
     require_once 'models/ampoule.php';
     if (@$_GET['confirm']==true) {
         $newAmpoule = new Ampoule;
         $newAmpoule->del(@$_SESSION['id']);
         $_SESSION['confirm']= true;    
-        header('Location: index.php');
+ 
     }
     
 } else{
     $_SESSION['ask']=true;
-    header('Location: index.php');
+ 
 }
-
+header('Location: index.php');
 
 
 ?>
